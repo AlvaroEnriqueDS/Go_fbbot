fd9f6f49bcf7608c41df225387955e8734eb574c
alvaro <alvaro_enrique_1997@hotmail.com>
2019-02-16 01:09:34 -0500

    se añade nuesvas estructuras y un boton para llamar
    este contenido responde ya automaticamente todo lo que le pongas por msn

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+        "bytes"
         "encoding/json"
+        "fmt"
         "io/ioutil"
         "log"
-	"net/http"
+        "net/http"
+        "strings"
 )
 
 //CON ESTO MONTAMOS UN SERVER
@@ -44,6 +47,117 @@ func fbwebhook(w http.ResponseWriter, r *http.Request) {
                 w.Write([]byte("Token no valido"))
                 return
         }
+        if r.Method == http.MethodPost{
+                rm := RequestMessage{}
+                err := json.NewDecoder(r.Body).Decode(&rm)
+                if err != nil {
+                        log.Println(err)
+                        return
+                }
+                if rm.Object == "page" {
+                        for _, entry := range rm.Entry{
+                                for _, message := range entry.Messaging{
+                                        messageRecived(message)
+                                }
+                        }
+                }
+                w.WriteHeader(http.StatusOK)
+        }
+}
+func messageRecived(m Messaging)  {
+        if m.Text != "" {
+                menu := "*Llamar\n" +
+                        "*Comprar\n" +
+                        "*Jugar\n" +
+                        "Escrriba una de estas respuestas para elegir una opción"
+                m.Text = strings.ToUpper(m.Text)
+                switch m.Text {
+                case "LLAMAR":
+                        sendCallMessage(m.Sender.ID)
+                case "COMPRAR":
+                        sendTextMessage(m.Sender.ID, "Aun no hemos habilitado la opcion de compra, consulta con otra opcion.\n" +menu)
+                case "JUGAR":
+                        sendTextMessage(m.Sender.ID, "Aun no hemos habilitado la opcion de jugar, consulta con otra opcion.\n" +menu)
+                default:
+                        sendTextMessage(m.Sender.ID, "Hola cómo estás, soy el bot de La Coraza :). Por ahora solo tenemos las siguientes opciones:\n" +menu)
+
+
+                }
+        }
+}
+
+func sendTextMessage(recipientID, text string)  {
+        rm := ResponseMessage{
+                Recipient{recipientID},
+                MessageContent{text, nil},
+        }
+        callSendAPI(rm)
+}
+
+func sendCallMessage(recipientID string)  {
+        button  := Buttons{
+                Type: "phone_number",
+                Title: "Contactanos",
+                Payload: "+51999999999",
+        }
+
+        element := Elements{
+                Title: "Gracias por preferirnos",
+                ImageUrl: "https://www.lacoraza.com/assets/images/sello.png",
+                ItemUrl: "www.lacoraza.com",
+                Subtitle: "Deseas llamarnos?",
+                Buttons: []Buttons{button},
+        }
+
+        payload := Payload{
+                TemplateType: "generic",
+                Elements: []Elements{element},
+        }
+
+        attachment := &Attachment{
+                Type: "template",
+                Payload: payload,
+        }
+
+        mc := MessageContent{Attachment: attachment}
+
+        rm := ResponseMessage{
+                Recipient: Recipient{recipientID},
+                MessageContent: mc,
+        }
+
+        callSendAPI(rm)
+}
+
+func callSendAPI(mensaje ResponseMessage)  {
+        m, err := json.Marshal(mensaje)
+        if err != nil {
+                log.Printf("Hubo un error al convertir el mensaje a JSON %v\n", err)
+                return
+        }
+
+        fu := fmt.Sprintf("%s?access_token=%s", config.FbUrl, config.FbToken)
+        req, err := http.NewRequest("POST", fu, bytes.NewBuffer(m))
+        if err != nil {
+                log.Printf("Hubo un error al crear el request %v\n", err)
+                return
+        }
+        req.Header.Set("Content-Type", "application/json")
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+                log.Printf("Hubo un error al ejecutar la peticion %v\n", err)
+                return
+        }
+        defer resp.Body.Close()
+
+        if resp.StatusCode == 200 {
+               log.Println("Respuesta enviada correctamente")
+                return
+        }
+
+        log.Println("Error al enviar la respuesta, status: ", resp.Status)
 }
 
 //structura de  configuracion
@@ -52,10 +166,12 @@ type Config struct {
 	CertPem string `json:"cert_pem"`
 	KeyPem  string `json:"key_pem"`
 	MyToken string `json:"my_token"`
+	FbToken string `json:"fb_token"`
+	FbUrl string `json:"fb_url"`
 }
-        //creamos una variable de la estructura Config
-        //esta variable nos servira para la configuracion
-        var config Config
+//creamos una variable de la estructura Config
+//esta variable nos servira para la configuracion
+var config Config
 
 //funcion que nos lee el archivo json
 func loadConfig() {
--- models.go
+++ models.go
@@ -38,4 +38,31 @@ type ResponseMessage struct {
 
 type MessageContent struct {
         Text string `json:"text,omitempty"`
+        Attachment *Attachment `json:"attachment,omitempty"`
+}
+
+type Attachment struct {
+        Type string `json:"type"`
+        Payload `json:"payload"`
+}
+
+type Payload struct {
+        Url string `json:"url,omitempty"`
+        TemplateType string `json:"template_type"`
+        Elements []Elements `json:"elements"`
+}
+
+type Elements struct {
+        Title string `json:"title"`
+        Subtitle string `json:"subtitle"`
+        ItemUrl string `json:"item_url"`
+        ImageUrl string `json:"image_url"`
+        Buttons []Buttons `json:"buttons"`
+}
+
+type Buttons struct {
+        Type string `json:"type"`
+        Url string `json:"url,omitempty"`
+        Title string `json:"title"`
+        Payload string `json:"payload"`
 }
